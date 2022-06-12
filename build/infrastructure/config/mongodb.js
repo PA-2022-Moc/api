f9ff11e878b8324972f7338e724ff229f72b42e9
Leakune <lfavier@live.fr>
2022-06-12 19:41:01 +0200

    remove useless variable

--- build/infrastructure/config/mongodb.js
+++ build/infrastructure/config/mongodb.js
@@ -3,11 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongoDBuri = exports.client = void 0;
-const mongodb_1 = require("mongodb");
+exports.mongoDBuri = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const mongoDBuri = process.env.MONGODB_URI || "";
 exports.mongoDBuri = mongoDBuri;
-const client = new mongodb_1.MongoClient(mongoDBuri);
-exports.client = client;
--- src/infrastructure/config/mongodb.ts
+++ src/infrastructure/config/mongodb.ts
@@ -1,9 +1,7 @@
-import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const mongoDBuri = process.env.MONGODB_URI || "";
-const client = new MongoClient(mongoDBuri);
 
-export { client, mongoDBuri };
+export { mongoDBuri };
