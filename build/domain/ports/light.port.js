2ae2f9618e629656b89c50b053b4d3efa26b88a9
Leakune <lfavier@live.fr>
2022-06-25 19:45:28 +0200

    ignore build directory

--- build/domain/ports/light.port.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ILightPort = void 0;
-class ILightPort {
-}
-exports.ILightPort = ILightPort;
--- build/router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const light_controller_1 = require("./infrastructure/controller/light.controller");
-function router(fastify, opt, next) {
-    const lightRoutes = new light_controller_1.LightController();
-    lightRoutes.router(fastify, opt, next);
-}
-exports.router = router;
